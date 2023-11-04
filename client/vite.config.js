289c6c38b70fc4aec5ef6d0ff933bf1dc2e85aed
JorgeLombana <lombanauci@gmail.com>
2023-11-04 15:24:03 -0500

    Complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{// config the server to don't have the cors problem
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
+      }
+    }
+  }
 })
